b5c620f9f3e15599f66f9cba4ad9c9fb50032720
Ubuntu <disi@ML-RefVm-985248.cvijydt2o24ebfe5tma10wmcef.ax.internal.cloudapp.net>
2023-06-26 12:53:02 +0000

    args parser

--- main.py
+++ main.py
@@ -1,9 +1,21 @@
 
 from map_elites import MAP_Elites
 from evolutionary_algorithm import EA_Config
+from argparse import ArgumentParser
 import os
 
 def main(): 
+    
+    parser = ArgumentParser()
+    parser.add_argument("-d", "--debug", action="store_true", help="Enable debug mode")
+    parser.add_argument("-nd", "--no-display", action="store_true", help="Disable pygame display")
+
+    args = parser.parse_args()
+    if args.debug:
+        EA_Config.DEBUG = True
+    if not args.no_display:
+        EA_Config.NO_DISPLAY = True
+
     if EA_Config.NO_DISPLAY: 
         os.environ["SDL_VIDEODRIVER"] = "dummy"
 
